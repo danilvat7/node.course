48d17951888278e703c034db740d7c4d290cafdf
Daniyil Vatahovych <danilvat7@gmail.com>
2019-01-16 23:51:39 +0200

    add geocode api

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,28 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const API_KEY = 'AIzaSyC76SCjRWUlSa11bn7d3aZmz4NYmjR8ekM';
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+const queryParams = encodeURIComponent(argv['address']);
 
 request({
-    url: 'https://reqres.in/api/users?page=1',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${queryParams}&key=${API_KEY}`,
     json: true
 }, (err, response, body) => {
-    console.log(JSON.stringify(body, undefined, 4));
-    
+    console.log(`Addres: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 })
