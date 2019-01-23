2a2dcf2f48d33498f869b5bc2000b75c104535fa
Daniyil Vatahovych <danilvat7@gmail.com>
2019-01-23 22:56:19 +0200

    worked on tests

--- node-test/utils/utils.js
+++ node-test/utils/utils.js
@@ -2,8 +2,20 @@ module.exports.add = (a, b) => {
     return a + b;
 };
 
+module.exports.asyncAdd = (a, b, cb) => {
+    setTimeout(() => {
+        cb(a + b);
+    }, 1000);
+};
+
 module.exports.square = (a) => {
     return a * a;
+};
+
+module.exports.asyncSquare = (a, cb) => {
+    setTimeout(() => {
+        cb(a * a);
+    }, 1000);
 };
 
 module.exports.setName = (user, fullName) => {
--- node-test/utils/utils.spec.js
+++ node-test/utils/utils.spec.js
@@ -6,9 +6,23 @@ it('should add two numbers', () => {
     expect(res).toBe(10);
 });
 
+it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+        expect(sum).toBe(7);
+        done();
+    })
+});
+
 it('should square a number', () => {
     const res = utils.square(5);
     expect(res).toBe(25);
+});
+
+it('should async square a number', (done) => {
+    utils.asyncSquare(5, (res) => {
+        expect(res).toBe(25);
+        done();
+    });
 });
 
 it('shoul verify first and last names are set', () => {
